7886f8ffb57d96741c73f214c407b33f1ded9127
purushottam kumar <purushottam.kumar@janitri.in>
2022-03-27 20:09:38 +0530

    bottom_nav

--- app/src/main/java/com/yesandroid/sqlite/MainActivity.java
+++ app/src/main/java/com/yesandroid/sqlite/MainActivity.java
@@ -1,78 +1,69 @@
 package com.yesandroid.sqlite;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
-import java.util.ArrayList;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import com.yesandroid.sqlite.Fragment.HomeFragment;
+import com.yesandroid.sqlite.Fragment.HospitalFragment;
+import com.yesandroid.sqlite.Fragment.NotifyFragment;
+import com.yesandroid.sqlite.Fragment.ShippingFragment;
+import com.yesandroid.sqlite.Helper.BottomNavigationViewHelper;
 
 public class MainActivity extends AppCompatActivity {
 
-    ArrayList<String> selectedItems;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        selectedItems=new ArrayList<String>();
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
 
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
-        ListView chl=(ListView) findViewById(R.id.checkable_list);
-        //set multiple selection mode
-        chl.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
-        String[] items={"English","Chinese","French","German","Italian","Khmer"};
-        //supply data itmes to ListView
-        ArrayAdapter<String> aa=new ArrayAdapter<String>(this,R.layout.checkable_list_layout,R.id.txt_title,items);
-        chl.setAdapter(aa);
-        //set OnItemClickListener
+                Fragment selectedFragment = null;
 
-        chl.setOnItemClickListener(new AdapterView.OnItemClickListener(){
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                // selected item
-                String selectedItem = ((TextView) view).getText().toString();
-                if(selectedItems.contains(selectedItem))
-                    selectedItems.remove(selectedItem); //remove deselected item from the list of selected items
-                else
-                    selectedItems.add(selectedItem); //add selected item to the list of selected items
+                switch (item.getItemId())   {
+                    case R.id.action_home:
+                        selectedFragment = HomeFragment.getInstance();
+                        break;
 
+                    case R.id.action_hospital:
+                        selectedFragment = HospitalFragment.getInstance();
+                        break;
+
+                    case R.id.action_shipping:
+                        selectedFragment = ShippingFragment.getInstance();
+                        break;
+
+                    case R.id.action_notify:
+                        selectedFragment = NotifyFragment.getInstance();
+                        break;
+                }
+
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.main_frame,selectedFragment);
+                transaction.commit();
+                return false;
             }
-
         });
-
-
+        setDefaultFragment();
     }
 
-
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        getMenuInflater().inflate(R.menu.main, menu);
-//        return true;
-//    }
-
-
-//    public void onStart()
-//    {
-//        super.onStart();
-//        //create an instance of ListView
-//
-//    }
-
-    public void showSelectedItems(View view){
-        String selItems="";
-        for(String item:selectedItems){
-            if(selItems=="")
-                selItems=item;
-            else
-                selItems+="/"+item;
-        }
-        Toast.makeText(this, selItems, Toast.LENGTH_LONG).show();
+    private void setDefaultFragment() {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.main_frame,HomeFragment.getInstance());
+        transaction.commit();
     }
-
 }
