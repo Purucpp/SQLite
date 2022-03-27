5d90f6edfd359919976deb310c17217b30bb0d87
purushottam kumar <purushottam.kumar@janitri.in>
2022-03-27 15:50:29 +0530

    checkbox

--- app/src/main/java/com/yesandroid/sqlite/MainActivity.java
+++ app/src/main/java/com/yesandroid/sqlite/MainActivity.java
@@ -2,45 +2,77 @@ package com.yesandroid.sqlite;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
-
-
+    ArrayList<String> selectedItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //final Dialog dialog = new Dialog(this);
-
-        /*
-        Dialog dialog = new Dialog(this);
-        WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
-        lp.copyFrom(dialog.getWindow().getAttributes());
-        lp.width = WindowManager.LayoutParams.MATCH_PARENT;
-        lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
-        lp.gravity = Gravity.BOTTOM;
-        lp.windowAnimations = R.style.DialogAnimation;
-        dialog.getWindow().setAttributes(lp);
-
-        */
+        selectedItems=new ArrayList<String>();
 
 
+        ListView chl=(ListView) findViewById(R.id.checkable_list);
+        //set multiple selection mode
+        chl.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        String[] items={"English","Chinese","French","German","Italian","Khmer"};
+        //supply data itmes to ListView
+        ArrayAdapter<String> aa=new ArrayAdapter<String>(this,R.layout.checkable_list_layout,R.id.txt_title,items);
+        chl.setAdapter(aa);
+        //set OnItemClickListener
+
+        chl.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // selected item
+                String selectedItem = ((TextView) view).getText().toString();
+                if(selectedItems.contains(selectedItem))
+                    selectedItems.remove(selectedItem); //remove deselected item from the list of selected items
+                else
+                    selectedItems.add(selectedItem); //add selected item to the list of selected items
+
+            }
+
+        });
 
 
-       // Dialog dialog = new Dialog(new ContextThemeWrapper(this, R.style.DialogSlideAnim));
-
-      //  getWindow().setBackgroundDrawableResource(android.R.color.background_dark);
     }
 
-    public void openb(View v)
-    {
-        ButtomSheet bt=new ButtomSheet();
-        bt.show(getSupportFragmentManager(),"example");
 
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.main, menu);
+//        return true;
+//    }
+
+
+//    public void onStart()
+//    {
+//        super.onStart();
+//        //create an instance of ListView
+//
+//    }
+
+    public void showSelectedItems(View view){
+        String selItems="";
+        for(String item:selectedItems){
+            if(selItems=="")
+                selItems=item;
+            else
+                selItems+="/"+item;
+        }
+        Toast.makeText(this, selItems, Toast.LENGTH_LONG).show();
     }
+
 }
