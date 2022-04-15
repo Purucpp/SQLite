1965555db35e20e5569972a3cfcb6b60d4a1cd9e
purushottam kumar <purushottam.kumar@janitri.in>
2022-04-15 15:08:08 +0530

    udp_server code

--- app/src/main/java/com/yesandroid/sqlite/MainActivity.java
+++ app/src/main/java/com/yesandroid/sqlite/MainActivity.java
@@ -15,32 +15,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //final Dialog dialog = new Dialog(this);
-
-        /*
-        Dialog dialog = new Dialog(this);
-        WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
-        lp.copyFrom(dialog.getWindow().getAttributes());
-        lp.width = WindowManager.LayoutParams.MATCH_PARENT;
-        lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
-        lp.gravity = Gravity.BOTTOM;
-        lp.windowAnimations = R.style.DialogAnimation;
-        dialog.getWindow().setAttributes(lp);
-
-        */
 
 
 
-
-       // Dialog dialog = new Dialog(new ContextThemeWrapper(this, R.style.DialogSlideAnim));
-
-      //  getWindow().setBackgroundDrawableResource(android.R.color.background_dark);
     }
 
-    public void openb(View v)
-    {
-        ButtomSheet bt=new ButtomSheet();
-        bt.show(getSupportFragmentManager(),"example");
 
-    }
 }
