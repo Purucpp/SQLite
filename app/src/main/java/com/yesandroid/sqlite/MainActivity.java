d8c4cd9b34e6dfc6053cb709bb4454d58f58e7d5
purushottam kumar <purushottam.kumar@janitri.in>
2022-04-15 15:13:11 +0530

    udp client send

--- app/src/main/java/com/yesandroid/sqlite/MainActivity.java
+++ app/src/main/java/com/yesandroid/sqlite/MainActivity.java
@@ -2,45 +2,101 @@ package com.yesandroid.sqlite;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import android.os.Bundle;
+import android.os.Handler;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
 
 
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView mTextViewReplyFromServer;
+    private EditText mEditTextSendMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //final Dialog dialog = new Dialog(this);
+        Button buttonSend = (Button) findViewById(R.id.btn_send);
 
-        /*
-        Dialog dialog = new Dialog(this);
-        WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
-        lp.copyFrom(dialog.getWindow().getAttributes());
-        lp.width = WindowManager.LayoutParams.MATCH_PARENT;
-        lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
-        lp.gravity = Gravity.BOTTOM;
-        lp.windowAnimations = R.style.DialogAnimation;
-        dialog.getWindow().setAttributes(lp);
+        mEditTextSendMessage = (EditText) findViewById(R.id.edt_send_message);
+        mTextViewReplyFromServer = (TextView) findViewById(R.id.tv_reply_from_server);
 
-        */
-
-
-
-
-       // Dialog dialog = new Dialog(new ContextThemeWrapper(this, R.style.DialogSlideAnim));
-
-      //  getWindow().setBackgroundDrawableResource(android.R.color.background_dark);
+        buttonSend.setOnClickListener(this);
     }
 
-    public void openb(View v)
-    {
-        ButtomSheet bt=new ButtomSheet();
-        bt.show(getSupportFragmentManager(),"example");
+    @Override
+    public void onClick(View v) {
 
+        switch (v.getId()) {
+
+            case R.id.btn_send:
+                sendMessage(mEditTextSendMessage.getText().toString());
+                break;
+        }
+    }
+
+    private void sendMessage(final String message) {
+
+        final Handler handler = new Handler();
+        Thread thread = new Thread(new Runnable() {
+
+            String stringData;
+
+            @Override
+            public void run() {
+
+                DatagramSocket ds = null;
+                try {
+                    ds = new DatagramSocket();
+                    // IP Address below is the IP address of that Device where server socket is opened.
+                    InetAddress serverAddr = InetAddress.getByName("xxx.xxx.xxx.xxx");
+                    DatagramPacket dp;
+                    dp = new DatagramPacket(message.getBytes(), message.length(), serverAddr, 9001);
+                    ds.send(dp);
+
+                    byte[] lMsg = new byte[1000];
+                    dp = new DatagramPacket(lMsg, lMsg.length);
+                    ds.receive(dp);
+                    stringData = new String(lMsg, 0, dp.getLength());
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    if (ds != null) {
+                        ds.close();
+                    }
+                }
+
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        String s = mTextViewReplyFromServer.getText().toString();
+                        if (stringData.trim().length() != 0)
+                            mTextViewReplyFromServer.setText(s + "\nFrom Server : " + stringData);
+
+                    }
+                });
+            }
+        });
+
+        thread.start();
     }
 }
