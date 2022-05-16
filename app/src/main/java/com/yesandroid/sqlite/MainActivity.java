09c675def39dd2fd356d16f25df9c8d10c2c9161
purushottam kumar <purushottam.kumar@janitri.in>
2022-05-16 21:39:42 +0530

    mvp

--- app/src/main/java/com/yesandroid/sqlite/MainActivity.java
+++ app/src/main/java/com/yesandroid/sqlite/MainActivity.java
@@ -1,46 +1,84 @@
 package com.yesandroid.sqlite;
 
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static android.view.View.GONE;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Contract.View {
 
+    // creating object of TextView class
+    private TextView textView;
 
+    // creating object of Button class
+    private Button button;
 
+    // creating object of ProgressBar class
+    private ProgressBar progressBar;
+
+    // creating object of Presenter interface in Contract
+    Contract.Presenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //final Dialog dialog = new Dialog(this);
+        // assigning ID of the TextView
+        textView = findViewById(R.id.textView);
 
-        /*
-        Dialog dialog = new Dialog(this);
-        WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
-        lp.copyFrom(dialog.getWindow().getAttributes());
-        lp.width = WindowManager.LayoutParams.MATCH_PARENT;
-        lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
-        lp.gravity = Gravity.BOTTOM;
-        lp.windowAnimations = R.style.DialogAnimation;
-        dialog.getWindow().setAttributes(lp);
+        // assigning ID of the Button
+        button = findViewById(R.id.button);
 
-        */
+        // assigning ID of the ProgressBar
+        progressBar = findViewById(R.id.progressBar);
 
+        // instantiating object of Presenter Interface
+        presenter = new Presenter(this, new Model());
 
-
-
-       // Dialog dialog = new Dialog(new ContextThemeWrapper(this, R.style.DialogSlideAnim));
-
-      //  getWindow().setBackgroundDrawableResource(android.R.color.background_dark);
+        // operations to be performed when
+        // user clicks the button
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                presenter.onButtonClick();
+            }
+        });
     }
 
-    public void openb(View v)
-    {
-        ButtomSheet bt=new ButtomSheet();
-        bt.show(getSupportFragmentManager(),"example");
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        presenter.onDestroy();
+    }
+
+    @Override
+    // method to display the Course Detail TextView
+    public void showProgress() {
+        progressBar.setVisibility(View.VISIBLE);
+        textView.setVisibility(View.INVISIBLE);
+    }
+
+    @Override
+    // method to hide the Course Detail TextView
+    public void hideProgress() {
+        progressBar.setVisibility(GONE);
+        textView.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    // method to set random string
+    // in the Course Detail TextView
+    public void setString(String string) {
+        textView.setText(string);
     }
 }
