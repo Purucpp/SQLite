dd6c3d016e1c9abc0a459718d05e92153838c3fd
Krishu143 <Krishna.k@krishitantra.com>
2023-03-22 15:47:38 +0530

    view binding

---
+++ app/src/main/java/com/yesandroid/sqlite/MainActivity2.java
@@ -0,0 +1,30 @@
+package com.yesandroid.sqlite;
+
+import android.os.Bundle;
+
+import com.yesandroid.sqlite.databinding.ActivityMain2Binding;
+
+public class MainActivity2 extends BaseActivity<ViewModel, ActivityMain2Binding> {
+
+    @Override
+    public int getLayout() {
+        return R.layout.activity_main2;
+    }
+
+    public int getViewModel() {
+        return BR.loginViewModel;
+    }
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
+    }
+}
---
+++ app/src/main/java/com/yesandroid/sqlite/ViewModel.java
@@ -0,0 +1,13 @@
+package com.yesandroid.sqlite;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.databinding.ViewDataBinding;
+import androidx.lifecycle.AndroidViewModel;
+
+public class ViewModel extends AndroidViewModel {
+    public ViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
