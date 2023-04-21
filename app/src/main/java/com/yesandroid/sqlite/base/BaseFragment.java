427f1d59947c94c8090a38f6fcc01bd8cb907d10
Krishu143 <Krishna.k@krishitantra.com>
2023-04-21 17:20:46 +0530

    base done

--- app/src/main/java/com/yesandroid/sqlite/base/BaseFragment.java
+++ app/src/main/java/com/yesandroid/sqlite/base/BaseFragment.java
@@ -1,5 +1,7 @@
 package com.yesandroid.sqlite.base;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +11,7 @@ import android.widget.Toast;
 import androidx.annotation.LayoutRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
 import androidx.databinding.DataBindingUtil;
 import androidx.databinding.ViewDataBinding;
 import androidx.fragment.app.Fragment;
@@ -16,12 +19,19 @@ import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.yesandroid.sqlite.R;
+
 import java.lang.reflect.ParameterizedType;
+import java.util.ArrayList;
+import java.util.List;
 
 public abstract class BaseFragment<V extends BaseViewModel, B extends ViewDataBinding> extends Fragment {
 
     protected V mViewModel;
     protected B mBinding;
+
+    private ProgressDialog mProgressDialog;
 
     protected abstract @LayoutRes
     int getLayout();
@@ -88,6 +98,166 @@ public abstract class BaseFragment<V extends BaseViewModel, B extends ViewDataBi
         super.onDestroy();
         getmViewModel().onCleared();
     }
+
+
+
+    public Fragment replaceFrag(int id, String className) {
+        return replaceFrag(id, className, null);
+    }
+
+
+
+    public Fragment createFragment(String className) {
+        return getChildFragmentManager().getFragmentFactory().instantiate(ClassLoader.getSystemClassLoader(), className);
+    }
+
+
+    public Fragment replaceFrag(int id, String className, Bundle bundle) {
+        Fragment frag = getChildFragmentManager().getFragmentFactory().instantiate(ClassLoader.getSystemClassLoader(), className);
+        if (bundle != null)
+            frag.setArguments(bundle);
+        getChildFragmentManager().beginTransaction()
+                .add(id, frag)
+                .commit();
+
+        return frag;
+    }
+
+
+    public void showAlertDialog(@NonNull String title, @NonNull String message) {
+        showAlertDialog(title, message, "ok", (dialog, which) -> dialog.dismiss(), "", null);
+    }
+
+
+    public void showAlertDialog(@NonNull String title, @NonNull String message, @NonNull String positiveText, DialogInterface.OnClickListener pClickListener) {
+        showAlertDialog(title, message, positiveText, pClickListener, "", null);
+    }
+
+
+    public void showAlertDialog(String title, String message, String positiveText, DialogInterface.OnClickListener pClickListener, String negative, DialogInterface.OnClickListener nClickListener) {
+        MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(getActivity(), R.style.MaterialAlertDialogOverlay);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        if (pClickListener != null)
+            builder.setPositiveButton(positiveText, pClickListener);
+
+        if (nClickListener != null) {
+            builder.setNegativeButton(negative, nClickListener);
+        }
+        builder.create().show();
+
+    }
+
+
+    public void finish() {
+        getActivity().finish();
+    }
+
+
+    public void showLoading(String title, String message, boolean determinate) {
+        hideLoading();
+        mProgressDialog = Utils.showLoadingDialog(getContext(), title, message, false);
+    }
+
+
+
+
+    public void hideLoading() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.cancel();
+        }
+    }
+
+
+    public void showLoading(String title, String message) {
+        showLoading(title, message, false);
+
+    }
+
+
+    public void showToast(int message) {
+        showToast(getString(message));
+
+    }
+
+    public void displayMessage(boolean success, String message) {
+        if (mProgressDialog != null && mProgressDialog.isShowing() && success)
+            mProgressDialog.dismiss();
+
+        if (!success && mProgressDialog.isShowing()) {
+            mProgressDialog.setMessage(message);
+        }
+
+    }
+
+    public void updateProgress(String message) {
+        displayMessage(false, message);
+
+    }
+
+
+    public void updateProgress(int number) {
+        if (mProgressDialog != null && !mProgressDialog.isIndeterminate()) {
+            mProgressDialog.setProgress(number);
+        }
+    }
+
+
+
+    public void showLoading(int title, int message) {
+        showLoading(getString(title), getString(message));
+    }
+
+
+    public void showLoading(int title, int message, boolean cancelable) {
+        showLoading(getString(title), getString(message), cancelable);
+    }
+
+    public void showAlertDialog(@StringRes int title, @StringRes int message) {
+        if (title == -1)
+            showAlertDialog("", getString(message));
+        else if (message == -1)
+            showAlertDialog(getString(title), "");
+        else
+            showAlertDialog(getString(title), getString(message));
+    }
+
+    /*
+    @Override
+    public final void dispatchMessageToActivity(int number) {
+        if (fragmentInteractions != null) {
+            for (FragmentInteraction fragmentInteraction : fragmentInteractions) {
+                fragmentInteraction.onMessageReceived(number);
+            }
+
+        }
+    }
+
+    @Override
+    public void dispatchObjectToActivity(Bundle message) {
+        if (fragmentInteractions != null) {
+            for (FragmentInteraction fragmentInteraction : fragmentInteractions) {
+                //fragmentInteraction.onObjectReceviced(message);
+            }
+
+        }
+    }
+
+
+
+
+    public void attachInteraction(@NonNull FragmentInteraction fragmentInteraction) {
+        this.fragmentInteractions.add(fragmentInteraction);
+    }
+
+    public void dettachInteraction(@NonNull FragmentInteraction fragmentInteraction) {
+        if (fragmentInteraction != null) {
+            fragmentInteractions.remove(fragmentInteraction);
+        }
+    }
+
+    */
+
 
 
 
